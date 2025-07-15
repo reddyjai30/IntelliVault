c06454086eac5776be3f812632b79b67ebe89492
reddyjai30 <reddyjai30@gmail.com>
2025-07-16 03:04:33 +0530

    contract verified

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -3,6 +3,12 @@ require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.20",
+  settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545"
@@ -10,7 +16,7 @@ module.exports = {
     blockdag: {
       url: "https://rpc.primordial.bdagscan.com",
       chainId: 1043,
-      accounts: [process.env.PRIVATE_KEY]
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
     }
   }
 };
